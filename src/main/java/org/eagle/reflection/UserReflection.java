7e2c674c1f582de0e88ead9e6325a54d12b764f6
q_cool <henry_love_1314@126.com>
2022-04-02 17:50:46 +0800

    夯实基础

--- src/main/java/org/eagle/reflection/UserReflection.java
+++ src/main/java/org/eagle/reflection/UserReflection.java
@@ -4,6 +4,7 @@ import org.eagle.entity.User;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 /**
  * @author ljj
@@ -16,14 +17,20 @@ public class UserReflection {
         final Class<?> clazz = Class.forName("org.eagle.entity.User");
 
         // 构造一个对象
-        final Constructor<?> constructor = clazz.getConstructor(Integer.class, String.class, Integer.class);
+        final Constructor<?> constructor = clazz.getDeclaredConstructor(Integer.class, String.class, Integer.class);
         final User henry = (User) constructor.newInstance(111, "henry", 12);
         System.out.println("===================================");
         System.out.println(henry);
         System.out.println("===================================");
 
         // 通过反射调用方法
-
+        // 使用 invoke 调用方法，激活，执行
+        // 参数：哪个对象 和 调用的参数
+        final Method setName = clazz.getDeclaredMethod("setName", String.class);
+        setName.invoke(henry,"stark");
+        System.out.println("===================================");
+        System.out.println(henry);
+        System.out.println("===================================");
     }
 
     public static void userHandler(String className) {
